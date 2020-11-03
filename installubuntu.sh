db5ac5a94265df2c550e0b5b2c1b99f6af8e1c71
Denis Mitin <denis.mitin.005@gmail.com>
2020-11-03 21:46:48 +0300

    Update installubuntu.sh

--- installubuntu.sh
+++ installubuntu.sh
@@ -1,5 +1,2 @@
 pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
-for(1, 10)
-do
-echo ENTER ./start-ubuntu.sh TO CONTINUE!!!
-done
+for i in {1..10}; do echo ENTER ./start-ubuntu.sh TO CONTINUE!!!; done
