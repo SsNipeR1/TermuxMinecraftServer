64b76d563d03d6477fd2ae34a23ea45220036792
Denis Mitin <denis.mitin.005@gmail.com>
2020-11-03 21:42:13 +0300

    Create installubuntu.sh

---
+++ installubuntu.sh
@@ -0,0 +1,5 @@
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+for(1, 10)
+do
+echo ENTER ./start-ubuntu.sh TO CONTINUE!!!
+done
